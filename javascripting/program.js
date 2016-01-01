9898b97b747c05471109bff68eb3ae3448ef132f
shoaib.g <shoaib.g@productivet.com>
2016-01-01 23:42:42 +0530

    javascript lessions after some passes

---
+++ javascripting/program.js
@@ -0,0 +1,12 @@
+// Ex-1
+//console.log('HELLO WORLD');
+
+//Ex-2
+// var total = 0;
+// for(var i = 2; i < process.argv.length; i++){
+//   if(typeof parseInt(process.argv[i]) === 'number')
+//     total += parseInt(process.argv[i]);
+// }
+// console.log(total);
+
+//Ex-3
